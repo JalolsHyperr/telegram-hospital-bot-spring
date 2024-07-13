8a628a95a7b3c97a288f92745917a591da8b3902
Jaloliddin Mustafoev <jalolshyper@icloud.com>
2024-07-12 17:00:47 -0800

    Initial commit with Spring Boot project and OpenAPI documentation

---
+++ src/main/java/com/example/telegram_hospital_bot/TelegramHospitalBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegram_hospital_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramHospitalBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramHospitalBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/telegram_hospital_bot/TelegramHospitalBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegram_hospital_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramHospitalBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
